6cc4d608e4b2f288af2829d2236eca6c94a356e6
PawanPawar11 <pawanpawar4369@gmail.com>
2025-09-25 23:38:39 +0530

    changed the order of env variable

--- server/src/index.js
+++ server/src/index.js
@@ -5,9 +5,13 @@ import { connectToDB } from "./config/db.js";
 import router from "./routes/urlRoutes.js";
 import cors from "cors";
 
-const PORT = process.env.PORT || 8001;
 const app = express();
 dotenv.config();
+const PORT = process.env.PORT || 8001; // the order of the environment variables also matters a lot, earlier I had this line at top, even
+// before express and because of having dotenv.config() function below the process.env.PORT number I was not able to read my port 5001, and it
+// was running on 5001. It was very hard to catch this issue on production though, it was my luck that I found it this early because I was
+// just checking out if everything is working fine on development side. And it turns out the postman was throwing error and when I opened
+// the log on vs code that's when I finally understood the issue!
 app.use(cors());
 app.use(express.json());
 connectToDB(process.env.MONGO_URI);
